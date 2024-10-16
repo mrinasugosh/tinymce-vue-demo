44cafabcedd23d1cc754bd4c9897c8325e0d9ebf
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-10-16 13:24:17 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Editor from '@tinymce/tinymce-vue'
+import { ref } from 'vue'
+const apiKey = import.meta.env.VITE_TINYMCE_API_KEY
+const value = ref('Hello <strong>world</strong>')
+</script>
+
+<template>
+  <Editor :api-key="apiKey" v-model="value" />
+  <div v-html="value"></div>
+</template>
